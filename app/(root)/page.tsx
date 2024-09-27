10dc3315a2dcd7fc6c4f153ad5f7044aac6fcd9a
keli chen <90227843+chavez777@users.noreply.github.com>
2024-09-27 03:26:24 -0400

    42:23

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div><Button>1111</Button></div>
+  )
+}
+
+export default Home
---
+++ app/Provider.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React, { ReactNode } from 'react'
+import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
+const Provider = ({ children }: { children: ReactNode}) => {
+  return (
+    <LiveblocksProvider  authEndpoint="/api/liveblocks-auth">
+      
+        <ClientSideSuspense fallback={<Loader/>}>
+          {children}
+        </ClientSideSuspense>
+      
+    </LiveblocksProvider>
+  )
+}
+
+export default Provider
