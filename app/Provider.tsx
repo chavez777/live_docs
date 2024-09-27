bb6b78c13bb94d401c8f5f3217008667fdb1957a
keli chen <90227843+chavez777@users.noreply.github.com>
2024-09-27 17:13:55 -0400

    1
    1

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { ReactNode } from 'react'
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
+import Loader from '@/components/Loader';
 const Provider = ({ children }: { children: ReactNode}) => {
   return (
     <LiveblocksProvider  authEndpoint="/api/liveblocks-auth">
