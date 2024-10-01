daaaf6ec2016446f3cd1bcca4eb0498317d9007d
keli chen <90227843+chavez777@users.noreply.github.com>
2024-09-30 22:04:34 -0400

    Update UserTypeSelector.tsx

--- components/UserTypeSelector.tsx
+++ components/UserTypeSelector.tsx
@@ -1,28 +1,31 @@
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-  
-  const UserTypeSelector = ({ userType, setUserType, onClickHandler }: UserTypeSelectorParams) => {
-    const accessChangeHandler = (type: UserType) => {
-      setUserType(type);
-      onClickHandler && onClickHandler(type);
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const UserTypeSelector = ({ userType, setUserType, onClickHandler }: UserTypeSelectorParams) => {
+  const accessChangeHandler = (type: UserType) => {
+    setUserType(type);
+    // Explicitly check if onClickHandler exists before calling
+    if (onClickHandler) {
+      onClickHandler(type);
     }
-  
-    return (
-      <Select value={userType} onValueChange={(type: UserType) => accessChangeHandler(type)}>
-        <SelectTrigger className="shad-select">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent className="border-none bg-dark-200">
-          <SelectItem value="viewer" className="shad-select-item">can view</SelectItem>
-          <SelectItem value="editor" className="shad-select-item">can edit</SelectItem>
-        </SelectContent>
-      </Select>
-    )
-  }
-  
-  export default UserTypeSelector
+  };
+
+  return (
+    <Select value={userType} onValueChange={(type: UserType) => accessChangeHandler(type)}>
+      <SelectTrigger className="shad-select">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent className="border-none bg-dark-200">
+        <SelectItem value="viewer" className="shad-select-item">can view</SelectItem>
+        <SelectItem value="editor" className="shad-select-item">can edit</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default UserTypeSelector;
